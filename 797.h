1a48be19dd33230e58e52a2021c9868c74b3598c
Le Zhang <le.zhang@momenta.ai>
2023-05-08 19:17:17 +0800

    chang

---
+++ 797.h
@@ -0,0 +1,22 @@
+//
+// Created by le.zhang on 2023/5/8.
+//
+
+#ifndef HOT100_797_H
+#define HOT100_797_H
+
+#include <vector>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+#endif //HOT100_797_H
